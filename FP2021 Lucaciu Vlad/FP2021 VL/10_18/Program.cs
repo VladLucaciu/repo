f2fb00aaaf461f21d3ef0ee8e33fa50778f5e4ad
Lucaciu Vlad <alinvlad87@gmail.com>
2021-11-13 18:53:44 +0200

    create strings inversul unui numar din 3 cifre

---
+++ FP2021 Lucaciu Vlad/FP2021 VL/10_18/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DistanceTo();
+        }
+        private static void DistanceTo()
+        {
+            Console.WriteLine("Introduceti km la care va aflati");
+            int km;
+
+            km = int.Parse(Console.ReadLine());
+            int distanta;
+
+            if (km > 60)
+            {
+                distanta = km - 60;
+            }
+            else
+            {
+                distanta = 60 - km;
+            }
+            Console.WriteLine($"Distanta este :{distanta}");
+            Console.WriteLine($"Distanta este: {Math.Abs(km - 60)}");
+        }
+    }
+}
---
+++ FP2021 Lucaciu Vlad/FP2021 VL/10_18a/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_18a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseDigits();
+        }
+        private static void ReverseDigits()
+        {
+            Console.WriteLine("Introduceti un numar natural format din maxim 3 cifre");
+            string line;
+            line = Console.ReadLine();
+            int n;
+
+            bool ret = int.TryParse(line, out n);
+            if (ret == false)
+            {
+                Console.WriteLine("Valoarea introdusa nu poate fi convertita in numar intreg ");
+                return;
+            }
+            if (n < 0 || n > 999)
+            {
+                Console.WriteLine("Numarul introdus nu este format din maxim 3 cifre ");
+                return;
+
+            }
+            if (n < 10)
+            {
+                Console.WriteLine($"n");
+            }
+            else
+            {
+                if (n < 100)
+                {
+                    Console.WriteLine($"{n % 10}{n / 10}");
+                }
+                else
+                {
+                    Console.WriteLine($"{n % 10}{n / 10 % 10}{n / 100}");
+                }
+            }
+        }
+    }
+}
