164ae78590f0c4b00c53b6b7a6e094aeb1f754ba
Lucaciu Vlad <alinvlad87@gmail.com>
2021-11-13 11:17:48 +0200

    Add 1st project

---
+++ ASC LucaciuVlad/LucaciuVlad_w1/LucaciuVlad_w1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucaciuVlad_w1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi to all those new to C# like miself!");
+        }
+    }
+}
